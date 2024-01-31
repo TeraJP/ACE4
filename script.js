7fbb5ae2a807e4f93358c3b26ecf260a7913559f
TeraJP <joaofarias.tera@gmail.com>
2024-01-30 23:51:39 -0300

    styles added

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ L.marker([-10.29559, -36.58207]).addTo(map)
 L.marker([-10.27796, -36.5644]).addTo(map)
     .bindPopup('Sequestro')
 
-L.marker([-10.29297,	-36.58247]).addTo(map)
+L.marker([-10.29297, -36.58247]).addTo(map)
     .bindPopup('Tráfico de drogas')
 
 L.marker([-10.28321, -36.56879]).addTo(map)
@@ -102,6 +102,350 @@ L.marker([-10.27796, -36.5644]).addTo(map)
 L.marker([-10.27996, -36.5659]).addTo(map)
     .bindPopup('Tentativa de homicídio')
 
+L.marker([-10.28516, -36.56269]).addTo(map)
+    .bindPopup('Tráfico')
+    .openPopup();
+
+L.marker([-10.29559, -36.58220]).addTo(map)
+    .bindPopup('Embriagado')
+    .openPopup();
+
+L.marker([-10.29238, -36.58400]).addTo(map)
+    .bindPopup('Mandado de Prisão')
+    .openPopup();
+
+L.marker([-10.29250, -36.58400]).addTo(map)
+    .bindPopup('Crack')
+    .openPopup();
+
+L.marker([-10.29570, -36.58225]).addTo(map)
+    .bindPopup('Lesão Corporal')
+    .openPopup();
+
+L.marker([-10.29005, -36.58570]).addTo(map)
+    .bindPopup('Confusão Generalizada')
+    .openPopup();
+
+L.marker([-10.27044, -36.56098]).addTo(map)
+    .bindPopup('Assasinado')
+    .openPopup();
+
+L.marker([-10.27796, -36.5647]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.28387, -36.56270]).addTo(map)
+    .bindPopup('Tentativa de Homicidio')
+    .openPopup();
+
+L.marker([-10.29076, -36.57975]).addTo(map)
+    .bindPopup('Tráfico de Drogas')
+    .openPopup();
+
+L.marker([-10.2803, -36.55312]).addTo(map)
+    .bindPopup('Atentado')
+    .openPopup();
+
+L.marker([-10.28881, -36.56386]).addTo(map)
+    .bindPopup('Assasinato')
+    .openPopup();
+
+L.marker([-10.28374, -36.54687]).addTo(map)
+    .bindPopup('Atentado')
+    .openPopup();
+
+    L.marker([-10.28881, -36.56400]).addTo(map)
+    .bindPopup('Tráfico de Drogas')
+    .openPopup();
+
+L.marker([-10.27715, -36.56491]).addTo(map)
+    .bindPopup('Acidente de Transito')
+    .openPopup();
+
+L.marker([-10.29322, -36.58365]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.29353, -36.8522]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.27456, -36.55659]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.28263, -36.5542]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.29494, -36.58232]).addTo(map)
+    .bindPopup('Disparo de Arma')
+    .openPopup();
+
+L.marker([-10.27557, -36.56052]).addTo(map)
+    .bindPopup('Assasinato')
+    .openPopup();
+
+    L.marker([-10.27557, -36.56060]).addTo(map)
+    .bindPopup('Homocidio')
+    .openPopup();
+
+L.marker([-10.27557, -36.56070]).addTo(map)
+    .bindPopup('Assasinato')
+    .openPopup();
+
+L.marker([-10.2864, -36.55607]).addTo(map)
+    .bindPopup('Veiculo Levado')
+    .openPopup();
+
+L.marker([-10.27967, -36.54709]).addTo(map)
+    .bindPopup('Homicidio')
+    .openPopup();
+
+L.marker([-10.28181, -36.56465]).addTo(map)
+    .bindPopup('Acidente de Transito')
+    .openPopup();
+
+L.marker([-10.29023, -36.58577]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.28381, -36.57353]).addTo(map)
+    .bindPopup('')
+    .openPopup();
+
+L.marker([-10.29559, -36.58237]).addTo(map)
+    .bindPopup('Assasinato')
+    .openPopup();
+
+L.marker([-10.27885, -36.56504]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.2802, -36.55510]).addTo(map)
+    .bindPopup('Homicidio')
+    .openPopup();
+
+L.marker([-10.28387, -36.56300]).addTo(map)
+    .bindPopup('Tentativa de Homicidio')
+    .openPopup();
+
+L.marker([-10.2943, -36.58092]).addTo(map)
+    .bindPopup('')
+    .openPopup();
+
+L.marker([-10.28231, -36.558]).addTo(map)
+    .bindPopup('Homicidio')
+    .openPopup();
+
+L.marker([-10.27796, -36.5660]).addTo(map)
+    .bindPopup('Maria da Penha')
+    .openPopup();
+
+L.marker([-10.27988, -36.656195]).addTo(map)
+    .bindPopup('Mandato de Prisão')
+    .openPopup();
+
+L.marker([-10.28181, -3656465]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.28421, -36.56524]).addTo(map)
+    .bindPopup('Estelionato')
+    .openPopup();
+
+L.marker([-10.28576, -36.56302]).addTo(map)
+    .bindPopup('Denuncia Caluniosa')
+    .openPopup();
+
+L.marker([-10.27967, -36.54720]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.28231, -36.558]).addTo(map)
+    .bindPopup('Maria da Penha')
+    .openPopup();
+
+L.marker([-10.28881, -36.56410]).addTo(map)
+    .bindPopup('Abuso')
+    .openPopup();
+
+L.marker([-10.28722, -36.56100]).addTo(map)
+    .bindPopup('Abuso')
+    .openPopup();
+
+L.marker([-10.29256, -36.57762]).addTo(map)
+    .bindPopup('Agressão')
+    .openPopup();
+
+L.marker([-10.28387, --36.56310]).addTo(map)
+    .bindPopup('')
+    .openPopup();
+
+L.marker([-10.29589, -36.5790]).addTo(map)
+    .bindPopup('Assasinado')
+    .openPopup();
+
+L.marker([-10.27254, -36.54494]).addTo(map)
+    .bindPopup('Acusado de Estrupo')
+    .openPopup();
+
+L.marker([-10.28387, --36.56320]).addTo(map)
+    .bindPopup('Atropelado')
+    .openPopup();
+
+L.marker([-10.28287, -36.55307]).addTo(map)
+    .bindPopup('Maria da Penha')
+    .openPopup();
+
+L.marker([-10.28151, -36.56284]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.29099, -36.57491]).addTo(map)
+    .bindPopup('Abuso')
+    .openPopup();
+
+L.marker([-10.2851, -36.55737]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.2855, -36.57558]).addTo(map)
+    .bindPopup('Abuso')
+    .openPopup();
+
+L.marker([--10.27796, -36.5670]).addTo(map)
+    .bindPopup('Abuso')
+    .openPopup();
+
+L.marker([-10.29238, -36.58410]).addTo(map)
+    .bindPopup('Tráfico de Drogas')
+    .openPopup();
+
+L.marker([-10.28259, -36.55442]).addTo(map)
+    .bindPopup('Morto')
+    .openPopup();
+
+L.marker([-10.28429, -36.55401]).addTo(map)
+    .bindPopup('Abuso')
+    .openPopup();
+
+L.marker([-10.28639, -36.5549]).addTo(map)
+    .bindPopup('Entorpecente')
+    .openPopup();
+
+L.marker([-10.27967, -36.56803]).addTo(map)
+    .bindPopup('Arma de Fogo')
+    .openPopup();
+
+L.marker([-10.27254, -36.54504]).addTo(map)
+    .bindPopup('Abuso')
+    .openPopup();
+
+L.marker([-10.29297, -36.58257]).addTo(map)
+    .bindPopup('')
+    .openPopup();
+
+L.marker([-10.28247, -36.55504]).addTo(map)
+    .bindPopup('Desapareceu')
+    .openPopup();
+
+L.marker([-10.2803, -36.55320]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.27967, -36.54750]).addTo(map)
+    .bindPopup('Atentado')
+    .openPopup();
+
+L.marker([-10.2855, -36.57570]).addTo(map)
+    .bindPopup('Assalto')
+    .openPopup();
+
+L.marker([-10.27456, -36.55670]).addTo(map)
+    .bindPopup('Atentado')
+    .openPopup();
+
+L.marker([-10.28259, -36.55450]).addTo(map)
+    .bindPopup('Arma de Fogo')
+    .openPopup();
+
+L.marker([-10.28387, --36.56330]).addTo(map)
+    .bindPopup('Tráfigo de Drogas')
+    .openPopup();
+
+L.marker([-10.28231, -36.568]).addTo(map)
+    .bindPopup('Assasinato')
+    .openPopup();
+
+L.marker([-10.26942, -36.55872]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.2802, -36.55520]).addTo(map)
+    .bindPopup('Arma de Fogo')
+    .openPopup();
+
+L.marker([-10.28247, -36.55510]).addTo(map)
+    .bindPopup('Tentativa de Homicidio')
+    .openPopup();
+
+L.marker([-10.28247, -36.55520]).addTo(map)
+    .bindPopup('Trafico de Drogas')
+    .openPopup();
+
+L.marker([-10.28287, -36.55320]).addTo(map)
+    .bindPopup('Ordem Policial')
+    .openPopup();
+
+L.marker([-10.29581, -36.57999]).addTo(map)
+    .bindPopup('Furto')
+    .openPopup();
+
+L.marker([-10.27557, -36.56080]).addTo(map)
+    .bindPopup('Desaparecimento')
+    .openPopup();
+
+L.marker([-10.27796, -36.5680]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.27914, -36.56]).addTo(map)
+    .bindPopup('Roubo')
+    .openPopup();
+
+L.marker([-10.29341, -36.57303]).addTo(map)
+    .bindPopup('Assasinato')
+    .openPopup();
+
+L.marker([-10.29076, -36.57990]).addTo(map)
+    .bindPopup('Tráfico de Drogas')
+    .openPopup();
+
+L.marker([-10.29303, -36.58161]).addTo(map)
+    .bindPopup('Tentativa de Homicidio')
+    .openPopup();
+
+L.marker([-10.29602, -36.57415]).addTo(map)
+    .bindPopup('Entorpecente')
+    .openPopup();
+
+L.marker([-10.29427, -36.57203]).addTo(map)
+    .bindPopup('Entorpecente')
+    .openPopup();
+
+L.marker([-10.28417, -36.55472]).addTo(map)
+    .bindPopup('Agreção a Companheira')
+    .openPopup();
+
+L.marker([-10.28471, -36.54867]).addTo(map)
+    .bindPopup('Homicidio')
+    .openPopup();
+
+L.marker([-10.28223, -36.57127]).addTo(map)
+    .bindPopup('Tráfico de Drogas')
+    .openPopup();
+
 /*L.marker([]).addTo(map)
     .bindPopup('')
     .openPopup();
@@ -109,283 +453,7 @@ L.marker([-10.27996, -36.5659]).addTo(map)
 L.marker([]).addTo(map)
     .bindPopup('')
     .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
+    
 L.marker([]).addTo(map)
     .bindPopup('')
     .openPopup();
@@ -397,15 +465,7 @@ L.marker([]).addTo(map)
 L.marker([]).addTo(map)
     .bindPopup('')
     .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
-L.marker([]).addTo(map)
-    .bindPopup('')
-    .openPopup();
-
+    
 L.marker([]).addTo(map)
     .bindPopup('')
     .openPopup();
